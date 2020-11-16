c28ba2daf1b17c558cc4abc8182e2d212b62cb3b
sudha <dhakshanya26@gmail.com>
2020-11-16 18:16:14 +0000

    added few more files

---
+++ CanteenSystem.Web/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CanteenSystem.Web.Models;
+using CanteenSystem.Web.ViewModel;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CanteenSystem.Web.Controllers
+{
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        private readonly CanteenSystemDbContext _context;
+        private readonly IIdentityServerInteractionService _interaction;
+        private readonly IClientStore _clientStore;
+        private readonly IAuthenticationSchemeProvider _schemeProvider;
+        private readonly IEventService _events;
+        public LoginController(CanteenSystemDbContext context,
+
+            IIdentityServerInteractionService interaction,
+            IClientStore clientStore,
+            IAuthenticationSchemeProvider schemeProvider,
+            IEventService events)
+        {
+            _context = context;
+            _interaction = interaction;
+            _clientStore = clientStore;
+            _schemeProvider = schemeProvider;
+            _events = events;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            return View(new LoginModel());
+        }
+    }
+}
--- CanteenSystem.Web/Program.cs
+++ CanteenSystem.Web/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace CanteenSystem.Web
 {
@@ -13,7 +15,30 @@ namespace CanteenSystem.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+               .MinimumLevel.Override("System", LogEventLevel.Warning)
+               .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+               .Enrich.FromLogContext()
+               .WriteTo.File(@"identityserver4_log.txt") 
+               .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+               .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host...");
+                CreateHostBuilder(args).Build().Run();
+                
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly."); 
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +46,7 @@ namespace CanteenSystem.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
